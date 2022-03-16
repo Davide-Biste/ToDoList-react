70f8cb3175c066c2328c9430dcc005e4f7ecd30d
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-03-16 14:30:19 +0100

    feat(login): implement react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Navbar from "./Component/Navbar/Navbar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Navbar/>
   </React.StrictMode>,
   document.getElementById('root')
 );
