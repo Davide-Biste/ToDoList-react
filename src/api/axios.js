f8bf6b318691bc44d4982ca52da60083cd2e7b3b
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-03-25 11:33:45 +0100

    correct axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8888";
+axios.defaults.baseURL = "https://davide-to-do.herokuapp.com";
 axios.defaults.headers["Content-Type"] = "application/x-www-form-urlencoded";
 
 // const token = JSON.parse(localStorage.getItem("token"));
